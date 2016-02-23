2fa6d6eb5650a745125d45f6a23d779b8a28b50b
pduleba <pduleba@pgs-soft.com>
2016-02-23 13:44:46 +0100

    Changes

--- adminUI/src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
+++ adminUI/src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
@@ -7,21 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class MyPluginServlet extends HttpServlet {
 
-public class MyPluginServlet extends HttpServlet
-{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8297159712077834837L;
 
-    private static final Logger log = LoggerFactory.getLogger(MyPluginServlet.class);
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-
-    {
+	{
 		resp.setContentType("text/html");
 		resp.getWriter().write("<html><body>Hello! You did it.</body></html>");
 
-    }
+	}
 
 }
